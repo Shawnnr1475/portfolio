7ad5eb824c7e467bca3852e7f912c1645aea4d45
Shawn Radebe <radebeshawnn@gmail.com>
2022-04-16 23:17:10 +0200

    adding footer

--- scripts.js
+++ scripts.js
@@ -1,15 +1,30 @@
 const projects = document.querySelectorAll(".project")
 const previews = document.querySelectorAll(".preview")
+const screenWidth = window.innerWidth
 
 projects.forEach(project => {
     project.addEventListener("click",()=>{
-        project.classList.toggle("expand")
-        if (project.classList.contains("expand")){
-            project.style.minWidth="600px"
+        if( screenWidth < 500){
+            project.classList.toggle("mask")
+            if (project.classList.contains("mask")){
+                project.firstElementChild.style.display = "none"
+            }
+            else{
+                project.firstElementChild.style.display = "flex"
+            }
         }
         else{
-            project.style.minWidth="300px"
+            project.classList.toggle("expand")
+            if (project.classList.contains("expand")){
+                project.style.minWidth="600px"
+            }
+            else{
+                project.style.minWidth="300px"
+            }
         }
     })
 });
+
+console.log(screenWidth)
+
 
